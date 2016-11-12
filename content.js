eaf795637d8f356c38a320e5bc793e69da0615e2
班章 <localhost>
2016-11-12 00:03:40 -0500

    added the storage option

--- content.js
+++ content.js
@@ -17,7 +17,7 @@
  * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  */
- 
+
 //establish a long-lived connection with the extension
 var port = chrome.extension.connect({
       name: "switch"
@@ -31,8 +31,37 @@ jQuery(document).ready(function($){
     jQuery('#check_input').change(function(){
     	if(jQuery('#check_input').is(":checked")==true){
     		 port.postMessage("on");
+    		 saveSettings("on");
     	} else {
     		port.postMessage("off");
+    		saveSettings("off");
     	}
     });
+    //setup the a tag jump
+    jQuery('body').on('click', 'a', function(){
+     chrome.tabs.create({url: $(this).attr('href')});
+     return false;
+   });
 });
+
+//save the switch settings in the database
+function saveSettings(msg){
+	chrome.storage.sync.set({'msg': msg}, function() {
+      // Notify that we saved.
+      console.log('Settings saved');
+    });
+}
+
+//initialize user settings 
+function init(){
+	console.log("init");
+	chrome.storage.sync.get("msg", function (obj) {
+    	console.log(obj.msg);
+    	if(obj.msg=="on"){
+    		jQuery("#check_input").attr("checked",true);
+    	}
+	});
+}
+
+//init the switch
+init();
